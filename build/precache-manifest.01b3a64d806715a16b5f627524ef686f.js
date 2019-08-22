b85d7be5509402418142e0e9faf78207cdc7a9d0
rafaelmonroy <rafaelmonroy1990@gmail.com>
2019-08-22 13:17:21 -0700

    using app.js

---
+++ build/precache-manifest.01b3a64d806715a16b5f627524ef686f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad97ad135a608edb7eb18096ead6dd07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83fdd9ce7ed64d4085b7",
+    "url": "/static/css/main.eb2658a2.chunk.css"
+  },
+  {
+    "revision": "88143b33720d4c8b8c70",
+    "url": "/static/js/2.2b37abeb.chunk.js"
+  },
+  {
+    "revision": "83fdd9ce7ed64d4085b7",
+    "url": "/static/js/main.0b6d405f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8e4084a01e5b8d9c314b670d3001c2ab",
+    "url": "/static/media/COWBELL1.8e4084a0.mp3"
+  },
+  {
+    "revision": "60ad7f85b087cd5ca1b40ac8d0c79576",
+    "url": "/static/media/CRASH1.60ad7f85.mp3"
+  },
+  {
+    "revision": "dcb0dcea9cd3fdcd70f8c69c212ba5e6",
+    "url": "/static/media/CRASH2.dcb0dcea.mp3"
+  },
+  {
+    "revision": "664f35ed5f056bc0250c4911a2984c0e",
+    "url": "/static/media/CRASH3.664f35ed.mp3"
+  },
+  {
+    "revision": "9c70f430a468b08b51fc8a30639d43e0",
+    "url": "/static/media/Drum1.9c70f430.mp3"
+  },
+  {
+    "revision": "ea7a416774a758938b20775d1e0ff565",
+    "url": "/static/media/HHCLOSE1.ea7a4167.mp3"
+  },
+  {
+    "revision": "0e326203103ab1c1f44697e9fd3a053a",
+    "url": "/static/media/HHCLOSE2.0e326203.mp3"
+  },
+  {
+    "revision": "4767e0ba721279cd1c9b92e3fc09ac9d",
+    "url": "/static/media/HHOPEN2.4767e0ba.mp3"
+  },
+  {
+    "revision": "d2597435ba302c53c5f7cace7bdc53c0",
+    "url": "/static/media/HICONGA.d2597435.mp3"
+  }
+]);
